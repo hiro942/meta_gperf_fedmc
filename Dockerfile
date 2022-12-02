e2383d610f445945541ac5b436363a4d0573e307
hiro942 <ee.Liulei@Gmail.com>
2022-12-02 18:23:05 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:alpine as builder
+WORKDIR /web
+COPY . .
+RUN yarn config set registry https://registry.npm.taobao.org && yarn && yarn build
+
+FROM nginx:alpine
+RUN rm /etc/nginx/conf.d/default.conf
+ADD deploy/nginx.conf /etc/nginx/conf.d/default.conf
+RUN ls -l
+COPY --from=builder /web/dist /usr/share/nginx/html
+EXPOSE 11191
+# RUN chmod -R 755 /usr/share/nginx/html
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    Check out
+    <a href="http://localhost:11191/meta/gperf" target="_blank"
+      >GPerf</a
+    >, the official Vue + Vite starter
+  </p>
+  <p>
+    Install
+    <a href="http://localhost:11191/meta/fedmc" target="_blank">FedMC</a>
+    in your IDE for a better DX
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
